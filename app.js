fddf2faae7cd7a9a9ef6ca42ab9d6e214a8e6718
ilKhr <58560537+ilKhr@users.noreply.github.com>
2020-05-13 02:16:03 +0300

    Edit static directory

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors())
 
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static('public'));
 
 app.use(function (req, res, next) {
     console.log(`${new Date()}: [${req.method}] ${req.url}`);
